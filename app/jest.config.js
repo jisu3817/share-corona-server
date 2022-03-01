c4a7d09f46175603c6a7e4c51dbe9aac7c109799
김지수 <kimjisu3817@gmail.com>
2022-03-01 14:42:45 +0900

    타입스크립트 환경 구축

---
+++ app/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: 'node',
+  bail: true,
+  verbose: true,
+  testRegex: '(/tests/.*[.](test|spec))\\.(ts|tsx|js)$',
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.js'],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  // clearMocks: true,
+  // collectCoverage: true,
+  // coverageDirectory: 'coverage',
+  // coverageProvider: 'v8',
+};
---
+++ app/src/test.ts
@@ -0,0 +1,11 @@
+interface User {
+  name: string;
+  age: number;
+}
+
+const user: User = {
+  name: 'jisu',
+  age: 22,
+};
+
+console.log(user.name);
