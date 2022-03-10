efb2b539b345aa76333ed518b5b61e943b88079a
김지수 <kimjisu3817@gmail.com>
2022-03-10 23:29:47 +0900

    Add(sign-up/test): 비밀번호 값이 undefined일 경우에 대한 테스트와 코드 구현 #2

--- app/src/models/services/user/user.ts
+++ app/src/models/services/user/user.ts
@@ -40,35 +40,35 @@ class User {
     const { id } = this.body;
 
     try {
-      if (id === undefined) return { success: false, msg: '아이디 값을 입력해주세요.' };
-      if (id.length < 5) return { success: false, msg: '아이디 값이 5자 이하입니다.' };
+      if (id === undefined) return { success: false, msg: '아이디를 입력해주세요.' };
+      if (id.length < 5) return { success: false, msg: '아이디가 5자 이하입니다.' };
 
       const DuplicatedId = await UserStorage.checkIdDuplication(id);
 
       if (DuplicatedId !== undefined) return { success: false, msg: '이미 존재하는 아이디입니다.' };
       // 토큰 생성 코드가 들어갈 곳.
-      return { success: true, msg: '아이디 유효 검사 완료' };
+      return { success: true, msg: '아이디 유효성 검사 완료' };
     } catch (err) {
       return { isError: true, errMsg: err, clientMsg: '알 수 없는 에러입니다. 서버 개발자에게 문의해주세요.' };
     }
   }
 
-  // async checkPassword(): Promise<response | error> {
-  //   const { password } = this.body;
+  async checkPassword(): Promise<response | error> {
+    const { password } = this.body;
 
-  //   try {
-  //     if (password === undefined) return { success: false, msg: '비밀번호를 입력해주세요.' };
-  //     if (userId.length < 5) return { success: false, msg: '아이디 값이 5자 이하입니다.' };
+    try {
+      if (password === undefined) return { success: false, msg: '비밀번호를 입력해주세요.' };
+      // if (userId.length < 5) return { success: false, msg: '아이디 값이 5자 이하입니다.' };
 
-  //     const DuplicatedId = await UserStorage.checkIdDuplication(userId);
+      // const DuplicatedId = await UserStorage.checkIdDuplication(userId);
 
-  //     if (DuplicatedId !== undefined) return { success: false, msg: '이미 존재하는 아이디입니다.' };
-  //     // 토큰 생성 코드가 들어갈 곳.
-  //     return { success: true, msg: '회원가입 완료' };
-  //   } catch (err) {
-  //     return { isError: true, errMsg: err, clientMsg: '알 수 없는 에러입니다. 서버 개발자에게 문의해주세요.' };
-  //   }
-  // }
+      // if (DuplicatedId !== undefined) return { success: false, msg: '이미 존재하는 아이디입니다.' };
+      // 토큰 생성 코드가 들어갈 곳.
+      return { success: true, msg: '회원가입 완료' };
+    } catch (err) {
+      return { isError: true, errMsg: err, clientMsg: '알 수 없는 에러입니다. 서버 개발자에게 문의해주세요.' };
+    }
+  }
 }
 
 export default User;
--- app/src/tests/root.test.js
+++ app/src/tests/root.test.js
@@ -4,34 +4,36 @@ import UserStorage from '../models/services/User/UserStorage';
 
 jest.mock('../models/services/User/UserStorage');
 
+let user;
+let req;
+
+beforeEach(() => {
+  req = {
+    body: {
+      id: '123',
+      password: '123',
+      checkPassword: '123',
+      email: '123@naver.com',
+      nickname: '123',
+      district: '123',
+    },
+  };
+
+  user = new User(req);
+});
+
 describe('checkId 함수 테스트', () => {
-  let user;
-  let req;
-
-  beforeEach(() => {
-    req = {
-      body: {
-        id: '123',
-        password: '123',
-        checkPassword: '123',
-        email: '123@naver.com',
-        nickname: '123',
-        district: '123',
-      },
-    };
-
-    user = new User(req);
-  });
-
   it('id값이 undefined라면 false 반환', async () => {
     delete req.body.id;
     const createId = await user.checkId();
-    expect(createId).toEqual({ success: false, msg: '아이디 값을 입력해주세요.' });
+
+    expect(createId).toEqual({ success: false, msg: '아이디를 입력해주세요.' });
   });
 
   it('id값의 길이가 5이하라면 false 반환', async () => {
     const createId = await user.checkId();
-    expect(createId).toEqual({ success: false, msg: '아이디 값이 5자 이하입니다.' });
+
+    expect(createId).toEqual({ success: false, msg: '아이디가 5자 이하입니다.' });
   });
 
   it('id값이 중복되는 데이터가 있다면 false 반환', async () => {
@@ -57,7 +59,6 @@ describe('checkId 함수 테스트', () => {
 
     UserStorage.checkIdDuplication = checkIdDuplication;
 
-    // const createId = await user.checkId();
     req.body.id = '12345';
 
     expect(user.checkId()).rejects.toThrow({
@@ -78,21 +79,22 @@ describe('checkId 함수 테스트', () => {
     req.body.id = '12345';
     const createId = await user.checkId();
 
-    expect(createId).toEqual({ success: true, msg: '아이디 유효 검사 완료' });
+    expect(createId).toEqual({ success: true, msg: '아이디 유효성 검사 완료' });
     expect(checkIdDuplication.mock.calls.length).toBe(1);
   });
+});
 
-  // it('password 길이가 8자 이하라면 throw error 반환', () => {
-  //   expect().toBeTruthy();
-  // });
+describe('checkPassword 함수 테스트', () => {
+  it('password값이 undefined라면 false 반환', async () => {
+    delete req.body.password;
+    const createPassword = await user.checkPassword();
 
-  // it('이메일이 중복된다면 throw error 반환', () => {
-  //   expect().toBeTruthy();
-  // });
+    expect(createPassword).toEqual({ success: false, msg: '비밀번호를 입력해주세요.' });
+  });
 
-  // it('모든 조건에 만족하는 id, password, email이라면 성공 메세지 반환', () => {
-  //   const isUnique = await stubUser.checkUniqueId('123');
+  // it('password 길이가 8자 이하라면 false 반환', () => {});
 
-  //   expect(isUnique).toEqual({});
-  // });
+  // it('password 값이 유효성 검사를 통과하지 못하는 경우 false 반환', () => {});
+
+  // it('password 값이 유효성 검사를 통과할 경우 false 반환', () => {});
 });
